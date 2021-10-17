2be0291c719b4e4743d45639d2a26a8a087c52a8
Melissa Consuelo Chavez Esquivel <a01284782@itesm.mx>
2021-10-17 08:36:33 -0500

    correción espacios

--- mejoraTEC.py
+++ mejoraTEC.py
@@ -1,14 +1,14 @@
-print('Hola, bienvendi@ a el juego. Escoge una de las siguientes opciones:\n1-Reglas del juego\n2-Jugar\n3-Recursos de aprendizaje\n4-Ver nivel\n0-Salir del juego')
-menu=1
+print('Hola, bienvendi@ a el juego. Escoge una de las siguientes opciones (escribir solo el número):\n1- Reglas del juego\n2-Jugar\n3- Recursos de aprendizaje\n4- Ver nivel\n0- Salir del juego')
+menu = 1
 
-pciencias='preguntasCiencias.txt'
-rciencias='respuestasCiencias.txt'
+pciencias = 'preguntasCiencias.txt'
+rciencias = 'respuestasCiencias.txt'
 
-plectura='preguntasEspañol.txt'
-rlectura='respuestasEspañol.txt'
+plectura = 'preguntasEspañol.txt'
+rlectura = 'respuestasEspañol.txt'
 
-pmate='preguntasMate.txt'
-rmate='respuestasMate.txt'
+pmate = 'preguntasMate.txt'
+rmate = 'respuestasMate.txt'
 
 listaCiencia = []
 listaMate = []
@@ -25,63 +25,65 @@ def menuPrincipal(x):
         checarNivel()
     
 def reglas():
-    archivo = open('Reglas.txt', 'r')
+    archivo = open('Reglas.txt', 'r', encoding = 'utf-8-sig')
     contenido = archivo.read()
     print(contenido)
     archivo.close()
+    print('Escoge nuevamente una de las siguientes opciones (escribir solo el número):\n1- Reglas del juego\n2- Jugar\n3- Recursos de aprendizaje\n4- Checar nivel\n0- Salir del juego')
 
 def jugar():
     print('Selecciona una opción\n1-Ciencia\n2-Matematicas\n3-Lectura')
     opt = int(input())
     print('ADVERTENCIA: favor de indicar su respuesta solamente con la letra correcta y en minuscula, gracias y buena suerte.')
-    if opt==1:
-        materias(pciencias,rciencias,opt)
-    elif opt==2:
-        materias(pmate,rmate,opt)
-    elif opt==3:
-        materias(plectura,rlectura,opt)
+    if opt == 1:
+        materias(pciencias, rciencias, opt)
+    elif opt == 2:
+        materias(pmate, rmate, opt)
+    elif opt == 3:
+        materias(plectura, rlectura, opt)
     else:
         print('Opción incorrecta')   
     
-def materias(x,y,z):
-    archivo=open(x,'r',encoding='utf-8-sig')
-    archivo1=open(y,'r',encoding='utf-8-sig') 
-    cont=0
+def materias(x, y, z):
+    archivo = open(x, 'r', encoding = 'utf-8-sig')
+    archivo1 = open(y, 'r', encoding = 'utf-8-sig') 
+    cont = 0
     for i in archivo:
-        print(i,end='')
-        respuesta=str(input())
-        a=archivo1.readline()
-        if respuesta==a[0]:
-            cont+=3
+        print(i, end='')
+        respuesta = str(input())
+        a = archivo1.readline()
+        if respuesta == a[0]:
+            cont += 3
         else:
-            cont-=1
+            cont -= 1
     archivo.close()
     archivo1.close()
     print('Tu puntaje fue de', cont)
-    if z==1:
+    if z == 1:
         listaCiencia.append(cont)
-        mat='Ciencias'
-    elif z==2:
+        mat = 'Ciencias'
+    elif z == 2:
         listaMate.append(cont)
         mat='Mate'
-    elif z==3:
+    elif z == 3:
         listaLec.append(cont)
-        mat='Lectura'
-    print('Has completado el módulo de',mat,'.Escoge nuevamente una de las siguientes opciones:\n1-Reglas del juego\n2-Jugar\n3-Recursos de aprendizaje\n4-Checar nivel\n0-Salir del juego')
+        mat = 'Lectura'
+    print('Has completado el módulo de', mat, '. Escoge nuevamente una de las siguientes opciones (escribir solo el número):\n1-Reglas del juego\n2-Jugar\n3-Recursos de aprendizaje\n4-Checar nivel\n0-Salir del juego')
 
 def checarNivel():
-    suma=0
+    suma = 0
     for i in range(len(listaCiencia)):
-        suma+=listaCiencia[i]
+        suma += listaCiencia[i]
     for z in range(len(listaLec)):
-        suma+=listaLec[z]
+        suma += listaLec[z]
     for p in range(len(listaMate)):
-        suma+=listaMate[p]
-    print('Su puntaje total es de',suma)
-    dic={'Ciencias':listaCiencia,'Mate':listaMate,'Lectura':listaLec,'Puntaje total':suma}
+        suma += listaMate[p]
+    print('Su puntaje total es de', suma)
+    dic={'Ciencias' : listaCiencia, 'Mate' : listaMate, 'Lectura' : listaLec, 'Puntaje total' : suma}
     print(dic)
+    print('Escoge nuevamente una de las siguientes opciones (escribir solo el número):\n1- Reglas del juego\n2- Jugar\n3- Recursos de aprendizaje\n4- Checar nivel\n0- Salir del juego')
 
-while menu!=0: 
-    menu=int(input())
+while menu != 0: 
+    menu = int(input())
     menuPrincipal(menu)
     
